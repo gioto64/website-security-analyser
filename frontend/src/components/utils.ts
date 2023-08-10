e721c410ccb4e9e951ed1866e8d10dfd14598a93
gioto64 <gpopescuioan@gmail.com>
2023-08-10 21:43:25 +0300

    add utils file

---
+++ frontend/src/components/utils.ts
@@ -0,0 +1,12 @@
+export const urlRegister = "http://0.0.0.0:8080/api/v1/signup";
+export const urlLogin = "http://0.0.0.0:8080/api/v1/login";
+export const urlSendFile = "http://0.0.0.0:8080/api/v1/add_file"
+export const urlSendUrl = "http://0.0.0.0:8080/api/v1/add_url"
+export const urlSearchFile = "http://0.0.0.0:8080/api/v1/file_info"
+export const urlSearchFiles = "http://0.0.0.0:8080/api/v1/user_files"
+export const urlSearchUrl = "http://0.0.0.0:8080/api/v1/url_info"
+export const urlSearchUrls = "http://0.0.0.0:8080/api/v1/user_urls"
+export const urlDelete = "http://0.0.0.0:8080/api/v1/delete_url"
+export const fileDelete = "http://0.0.0.0:8080/api/v1/delete_file"
+export const deleteAccount = "http://0.0.0.0:8080/api/v1/delete_user"
+export const googleApiKey = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=insertApiKey"
